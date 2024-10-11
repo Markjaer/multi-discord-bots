045ca88fab059809e4cce3e5aab7b710d855be39
M.K.E <markjaerttv@gmail.com>
2024-10-11 22:26:03 +0200

    master

--- config.js
+++ config.js
@@ -142,7 +142,148 @@ module.exports = {
                     }
                 }
             }
-        }
+        },
+        // { // bot has to be created on https://discord.com/developers/applications
+        //     name: "Bot name...",
+        //     website: "Website...",
+        //     token: "Bot token...",
+        //     botid: "Bot client id...",
+        //     serverid: "Server id...",
+        //     prefix: "!", // can be "!p" then you can "!p clear" or "!p ban <user>"
+        //     maincolor: "#FFFFFF", // hex only with # like this "#FFFFFF"
+        //     emoji: "logo", // emoji name thats has been uploaded from discord server like "logo" or "emoji_kappa"
+        //     hooks: {
+        //         tickets: {
+        //             label: "Label...",
+        //             headerimage: "https://example.com/example.png",
+        //             title: "Title...",
+        //             content: "Description...",
+        //             buttons: [
+        //                 {
+        //                     label: "Support",
+        //                     style: "success",
+        //                     emoji: "stop",
+        //                     headerimage: "https://example.com/example.png",
+        //                     title: "",
+        //                     content: "Description..."
+        //                 },
+        //                 {
+        //                     label: "Support",
+        //                     style: "success",
+        //                     emoji: "success",
+        //                     headerimage: "https://example.com/example.png",
+        //                     title: "",
+        //                     content: "Description..."
+        //                 },
+        //                 {
+        //                     label: "Support",
+        //                     style: "success",
+        //                     emoji: "error",
+        //                     headerimage: "https://example.com/example.png",
+        //                     title: "",
+        //                     content: "Description..."
+        //                 },
+        //                 {
+        //                     label: "Support",
+        //                     style: "success",
+        //                     emoji: "gun",
+        //                     headerimage: "https://example.com/example.png",
+        //                     title: "",
+        //                     content: "Description..."
+        //                 },
+        //             ]
+        //         },
+        //         claim: [
+        //             {
+        //                 label: "Label...",
+        //                 headerimage: "https://example.com/example.png",
+        //                 title: "Title...",
+        //                 content: "Description...",
+        //                 roles: [ // max 5 buttons and use only theese styles
+        //                     {label: "Label...", id: "00000000000000000", buttonstyle: "success", emojiid: "", link: ""},
+        //                     // {label: "Primary", id: "00000000000000000", buttonstyle: "primary", emojiid: "1290904172685688894", link: ""},
+        //                     // {label: "Secondary", id: "00000000000000000", buttonstyle: "secondary", emojiid: "1290904172685688894", link: ""},
+        //                     // {label: "Success", id: "00000000000000000", buttonstyle: "success", emojiid: "1290904172685688894", link: ""},
+        //                     // {label: "Danger", id: "00000000000000000", buttonstyle: "danger", emojiid: "1290904172685688894", link: ""},
+        //                     // {label: "Link", id: "00000000000000000", buttonstyle: "link", emojiid: "", link: "https://aimmaster.info"},
+        //                 ]
+        //             }
+        //         ],
+        //         channels: [
+        //             {
+        //                 enabled: true,
+        //                 text: "🎧 {membercount} members {req.name} {req.description}",
+        //                 interval: 60, // seconds: 60 is 1 minutes
+        //                 custom: "https://example.com/json.json", // api website to get json data like "{name: "test", description: "test"}" and use {req.[data from json]}
+        //                 channelids: [
+        //                     "00000000000000000"
+        //                 ]
+        //             }
+        //         ],
+        //         presence: {
+        //             enabled: true,
+        //             interval: 10, // seconds: 60 is 1 minutes
+        //             activities: [
+        //                 {
+        //                     type: ActivityType.Watching,
+        //                     text: "{membercount} members on discord {req.name} {req.description}",
+        //                     custom: "https://example.com/json.json", // api website to get json data like "{name: "test", description: "test"}" and use {req.[data from json]}
+        //                     status: "online"
+        //                 }
+        //             ]
+        //         },
+        //         logs: {
+        //             infochannelid: "00000000000000000",
+        //             ready: {
+        //                 enabled: true,
+        //                 channeltext: "**{bot}** is online!", // sending success
+        //                 pingtext: "**{bot}** is online!", // sending success
+        //                 pingmemberids: [
+        //                     "00000000000000000"
+        //                 ],
+        //                 channelids: [
+        //                     "00000000000000000"
+        //                 ]
+        //             },
+        //             offline: {
+        //                 enabled: true,
+        //                 backonlinetext: "**{server} is back online again!**\n{timestamp}", // sending success
+        //                 offlinetext: "**{server} is offline!**\nThere might be a error, you have to run the bot manual!\n{timestamp}", // sending error
+        //                 pingmemberids: [
+        //                     "00000000000000000"
+        //                 ],
+        //                 channelids: [
+        //                     "00000000000000000"
+        //                 ]
+        //             },
+        //             join: {
+        //                 enabled: true,
+        //                 channeltext: "{emoji} Yooo, {usertag} welcome to **{server}**", // sending main
+        //                 pingtext: "{usertag} has joined **{server}**", // sending success
+        //                 pingmemberids: [
+        //                     "00000000000000000"
+        //                 ],
+        //                 channelids: [
+        //                     "00000000000000000"
+        //                 ],
+        //                 addroles: [
+        //                     "00000000000000000"
+        //                 ]
+        //             },
+        //             leave: {
+        //                 enabled: true,
+        //                 channeltext: "{emoji} Noooo, {usertag} left **{server}**", // sending main
+        //                 pingtext: "{usertag} left **{server}**", // sending warning
+        //                 pingmemberids: [
+        //                     "00000000000000000"
+        //                 ],
+        //                 channelids: [
+        //                     "00000000000000000"
+        //                 ]
+        //             }
+        //         }
+        //     }
+        // }
     ],
 
     colors: {
